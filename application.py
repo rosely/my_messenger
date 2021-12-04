bb91477cdcb82895d8c18a66056b0bd4b8d7d31a
rosely <x21121206@student.ncirl.ie>
2021-12-04 17:32:42 +0000

    cognito

--- application.py
+++ application.py
@@ -8,23 +8,55 @@ from flask_jwt_extended import (
     verify_jwt_in_request,
     get_jwt_identity,
 )
+import json
+import os
+import requests
+
+# def get_cognito_public_keys():
+#     region = 'us-east-1'
+#     pool_id = 'us-east-1_t2focHvIB'
+#     url = f"https://rosely-web-chat.auth.us-east-1.amazoncognito.com/{pool_id}/.well-known/jwks.json"
+    
+
+#     resp = requests.get(url)
+#     return json.dumps(json.loads(resp.text)["keys"][1])
 
 
 app = Flask(__name__)
-app.config.from_object("config")
+# app.config.from_object("config")
+# app.config["JWT_PUBLIC_KEY"] = RSAAlgorithm.from_jwk(get_cognito_public_keys())
+app.config['AWS_DEFAULT_REGION'] = 'us-east-1'
+app.config['AWS_COGNITO_DOMAIN'] = 'https://rosely-web-chat.auth.us-east-1.amazoncognito.com/'
+app.config['AWS_COGNITO_USER_POOL_ID'] = 'us-east-1_t2focHvIB'
+app.config['AWS_COGNITO_USER_POOL_CLIENT_ID'] = '77bh0lg75rdu1bhseuhkvsjq9q'
+app.config['AWS_COGNITO_USER_POOL_CLIENT_SECRET'] = '10g2o3qsc6iqi3id2f77tgfdpd6m3p2m4o6436ka8n8ki81pqrud'
+app.config['AWS_COGNITO_REDIRECT_URL'] = 'https://100cff30983e458e97902464ea0ffb37.vfs.cloud9.us-east-1.amazonaws.com/loggedin'
+
+app.config['JWT_TOKEN_LOCATION'] = ["cookies"]
+app.config['JWT_COOKIE_SECURE'] = True
+
+# We're ok to set this off, as Cognito OAuth state provides protection
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+app.config['JWT_ALGORITHM'] = "RS256"
+app.config['JWT_IDENTITY_CLAIM'] = "sub"
+
+def get_cognito_public_keys():
+    region =  'us-east-1'
+    pool_id = 'us-east-1_t2focHvIB'
+    url = f"https://cognito-idp.{region}.amazonaws.com/{pool_id}/.well-known/jwks.json"
+
+    resp = requests.get(url)
+    return json.dumps(json.loads(resp.text)["keys"][1])
+    
 app.config["JWT_PUBLIC_KEY"] = RSAAlgorithm.from_jwk(get_cognito_public_keys())
+
+
+
 
 CORS(app)
 aws_auth = AWSCognitoAuthentication(app)
 jwt = JWTManager(app)
 
-def get_cognito_public_keys():
-    region = os.environ["AWS_REGION"]
-    pool_id = os.environ["AWS_COGNITO_USER_POOL_ID"]
-    url = f"https://cognito-idp.{region}.amazonaws.com/{pool_id}/.well-known/jwks.json"
-
-    resp = requests.get(url)
-    return json.dumps(json.loads(resp.text)["keys"][1])
 
 
 @app.route("/")
@@ -49,6 +81,15 @@ def logged_in():
 def protected():
     verify_jwt_in_request(optional=True)
     if get_jwt_identity():
+        print("OK!!")
         return render_template("secret.html")
     else:
+        print("Deu ruim!!")
         return redirect(aws_auth.get_sign_in_url())
+        
+        
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-AWS_DEFAULT_REGION = os.environ["AWS_REGION"]
+AWS_DEFAULT_REGION = 'us-east-1'
 JWT_TOKEN_LOCATION = ["cookies"]
 JWT_COOKIE_SECURE = True
 
@@ -9,12 +9,16 @@ JWT_COOKIE_CSRF_PROTECT = False
 JWT_ALGORITHM = "RS256"
 JWT_IDENTITY_CLAIM = "sub"
 
-SECRET_KEY = os.environ["SECRET_KEY"]
-JWT_PRIVATE_KEY = os.environ["JWT_PRIVATE_KEY"]
-#  We're using Cognito to generate keys, so this is never used
-JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
-AWS_COGNITO_DOMAIN = os.environ["AWS_COGNITO_DOMAIN"]
-AWS_COGNITO_USER_POOL_ID = os.environ["AWS_COGNITO_USER_POOL_ID"]
-AWS_COGNITO_USER_POOL_CLIENT_ID = os.environ["AWS_COGNITO_USER_POOL_CLIENT_ID"]
-AWS_COGNITO_USER_POOL_CLIENT_SECRET = os.environ["AWS_COGNITO_USER_POOL_CLIENT_SECRET"]
-AWS_COGNITO_REDIRECT_URL = os.environ["SITE_URL"] + "/loggedin"
+# SECRET_KEY = os.environ["SECRET_KEY"]
+# JWT_PRIVATE_KEY = os.environ["JWT_PRIVATE_KEY"]
+# #  We're using Cognito to generate keys, so this is never used
+# JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+
+
+
+
+AWS_COGNITO_DOMAIN = 'https://rosely-web-chat.auth.us-east-1.amazoncognito.com/'
+AWS_COGNITO_USER_POOL_ID = 'us-east-1_t2focHvIB'
+AWS_COGNITO_USER_POOL_CLIENT_ID = '77bh0lg75rdu1bhseuhkvsjq9q'
+AWS_COGNITO_USER_POOL_CLIENT_SECRET = '10g2o3qsc6iqi3id2f77tgfdpd6m3p2m4o6436ka8n8ki81pqrud'
+AWS_COGNITO_REDIRECT_URL = '/loggedin'
