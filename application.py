1364a75d583e335929d242b3861da30370c1d802
Rosely Nakahara <roselynakahara@gmail.com>
2021-12-08 09:32:18 -0300

    update

--- application.py
+++ application.py
@@ -8,19 +8,20 @@ from flask_jwt_extended import (
     set_access_cookies,
     verify_jwt_in_request,
     get_jwt_identity,
+    decode_token,
 )
 import json
 import os
 import requests
 
-# def get_cognito_public_keys():
-#     region = 'us-east-1'
-#     pool_id = 'us-east-1_t2focHvIB'   
-#     url = f"https://rosely-web-chat.auth.us-east-1.amazoncognito.com/{pool_id}/.well-known/jwks.json"
+def get_cognito_public_keys():
+    region = 'us-east-1'
+    pool_id = 'us-east-1_t2focHvIB'
+    url = f"https://rosely-web-chat.auth.us-east-1.amazoncognito.com/{pool_id}/.well-known/jwks.json"
     
 
-#     resp = requests.get(url)
-#     return json.dumps(json.loads(resp.text)["keys"][1])
+    resp = requests.get(url)
+    return json.dumps(json.loads(resp.text)["keys"][1])
 
 
 app = Flask(__name__)
@@ -31,6 +32,7 @@ app.config['AWS_COGNITO_DOMAIN'] = 'https://rosely-web-chat.auth.us-east-1.amazo
 app.config['AWS_COGNITO_USER_POOL_ID'] = 'us-east-1_t2focHvIB'
 app.config['AWS_COGNITO_USER_POOL_CLIENT_ID'] = '77bh0lg75rdu1bhseuhkvsjq9q'
 app.config['AWS_COGNITO_USER_POOL_CLIENT_SECRET'] = '10g2o3qsc6iqi3id2f77tgfdpd6m3p2m4o6436ka8n8ki81pqrud'
+# app.config['AWS_COGNITO_REDIRECT_URL'] = 'https://100cff30983e458e97902464ea0ffb37.vfs.cloud9.us-east-1.amazonaws.com/loggedin'
 app.config['AWS_COGNITO_REDIRECT_URL'] = 'https://100cff30983e458e97902464ea0ffb37.vfs.cloud9.us-east-1.amazonaws.com/loggedin'
 
 app.config['JWT_TOKEN_LOCATION'] = ["cookies"]
@@ -48,7 +50,7 @@ def get_cognito_public_keys():
 
     resp = requests.get(url)
     return json.dumps(json.loads(resp.text)["keys"][1])
-    
+
 app.config["JWT_PUBLIC_KEY"] = RSAAlgorithm.from_jwk(get_cognito_public_keys())
 
 
@@ -57,7 +59,7 @@ app.config["JWT_PUBLIC_KEY"] = RSAAlgorithm.from_jwk(get_cognito_public_keys())
 CORS(app)
 aws_auth = AWSCognitoAuthentication(app)
 jwt = JWTManager(app)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 
@@ -74,7 +76,8 @@ def login():
 @app.route("/loggedin", methods=["GET"])
 def logged_in():
     access_token = aws_auth.get_access_token(request.args)
-    resp = make_response(redirect(url_for("protected")))
+    username = decode_token(access_token).get("username")
+    resp = make_response(redirect(url_for("protected", username = username)))
     set_access_cookies(resp, access_token, max_age=30 * 60)
     return resp
 
@@ -83,10 +86,10 @@ def logged_in():
 def protected():
     verify_jwt_in_request(optional=True)
     if get_jwt_identity():
-        print("OK!!")
+        # print("access_token: " + aws_auth.get_access_token(request.args))
         # username = request.args.get('username')
         # room = request.args.get('room')
-        username = "rosely"
+        username = request.args.get('username')
         room = "friend1"
         return render_template("chat.html", username=username, room=room)
     else:
@@ -113,7 +116,6 @@ def handle_send_message_event(data):
 
 @socketio.on('join_room')
 def handle_join_room_event(data):
-    print("oioioioioioioioioioioioi")
     app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
     join_room(data['room'])
     socketio.emit('join_room_announcement', data, room=data['room'])
@@ -129,6 +131,6 @@ def handle_leave_room_event(data):
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.debug = True
-    app.run()
+    # app.debug = True
+    # app.run()
     socketio.run(app, debug=True)
